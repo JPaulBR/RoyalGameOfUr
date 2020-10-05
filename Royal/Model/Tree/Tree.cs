45c66d320485de5013ed5c126211da8c84c52fb1
Jean Paul Barrit <jpbr25@yahoo.com>
2020-10-05 15:31:33 -0600

    Update tree

--- Royal/Model/Tree/Tree.cs
+++ Royal/Model/Tree/Tree.cs
@@ -18,9 +18,45 @@ namespace Royal.Model.Tree
         {
             for (int i = 0; i < node.NoChildren; i++)
             {
-                node.Child[i].printNode();
+                //Console.WriteLine(node.Child[i].Data);
+                if (node.Child[i].NoChildren > 0)
+                {
+                    foreach (TreeNode j in node.Child[i].Child)
+                    {
+                        Console.Write(j.Data + "->");
+                    }
+                    Console.WriteLine("");
+                }
                 seeChildren(node.Child[i]);
             }
+        }
+
+        public List<TreeNode> childrenList(TreeNode node)
+        {
+            List<TreeNode> list = new List<TreeNode>();
+            foreach (TreeNode i in node.Child)
+            {
+                list.Add(i);
+            }
+            foreach (TreeNode j in list)
+            {
+                Console.Write(j.Data + "->");
+            }
+            return list;
+        }
+
+        public List<int> getLeafs(TreeNode node)
+        {
+            List<int> id_numbers = new List<int>();
+            for (int i = 0; i < node.NoChildren; i++)
+            {
+                if (node.Child[i].NoChildren == 0)
+                {
+                    id_numbers.Add(node.Child[i].Data);
+                }
+                getLeafs(node.Child[i]);
+            }
+            return id_numbers;
         }
 
         public void AddChild(TreeNode pNode, int data, int father)
